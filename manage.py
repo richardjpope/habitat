e1f7c350a8bbce2358713a3f2d825d8d4737197a
Richard Pope <richardpope@Richards-MacBook-Air.local>
2013-09-10 23:20:46 +0100

    stuff

--- manage.py
+++ manage.py
@@ -1,19 +1,34 @@
+import urllib
 from flask import Flask
 from flask.ext.script import Manager, Option, Command
 import shapefile
 import models
 import os
+import zipfile
 
 app = Flask(__name__)
+data_dir = data_dir = '%s/import-data' % os.path.dirname(os.path.abspath(__file__))
 
 class DownloadOSM(Command):
 
     def run(self):
 
-        print "hello"
-        data_dir = '%s/import-data' % os.path.dirname(os.path.abspath(__file__))
+        #create download directory if does not exist
         if not os.path.exists(data_dir):
             os.makedirs(data_dir)
+            print "Created download directory at %s" % data_dir
+
+        #download zip files
+        osm_zipped_urls = ['http://download.geofabrik.de/europe/great-britain/england/greater-london-latest.shp.zip',]
+        for url in osm_zipped_urls:
+
+            file_name = url.split('/')[len(url.split('/')) -1]
+            urllib.urlretrieve(url, "%s/%s" % (data_dir, file_name))
+
+            #upzip
+            osm_zipped_file = zipfile.ZipFile("%s/%s" % (data_dir, file_name), 'r')
+            osm_zipped_file.extractall(data_dir)
+            osm_zipped_file.close()
 
 class ImportOSM(Command):
 
@@ -24,10 +39,7 @@ class ImportOSM(Command):
 
         print "Starting to read shape files"
         #https://pypi.python.org/pypi/pyshp#reading-shapefiles
-        dir = os.path.dirname(os.path.abspath(__file__))
-        path = os.path.join(dir, "import-data/greater-london-latest/buildings")
-        print path
-        shape_reader = shapefile.Reader(path)
+        shape_reader = shapefile.Reader("%s/%s" % (data_dir, "buildings"))
         records = shape_reader.records()
         shapes = shape_reader.shapes()
 
